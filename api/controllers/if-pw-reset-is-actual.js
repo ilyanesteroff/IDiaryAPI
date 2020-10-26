83445dc6209be96e740ef00e6045c6a1c612c232
Ilya Nesterov <j.nesterov1709@gmail.com>
2020-10-26 17:39:16 +0200

    one more fix

--- api/controllers/if-pw-reset-is-actual.js
+++ api/controllers/if-pw-reset-is-actual.js
@@ -6,14 +6,11 @@ module.exports = async function(req, res) {
     const { token } = req.body
     if(!token) return res.status(400).json({ error: 'Token is missing' })
     
-    const settings = await UserSettings.getSpecificFields(
-      {"resetPassword.token" : token}, 
-      { resetPassword: 1 } 
-    )
+    const settings = await UserSettings.getSpecificFields( { "resetPassword.token" : token }, { resetPassword: 1 } )
     if(!settings) return res.status(404).json('User not found')
-    if(settings.resetPassword.bestBefore < new Date()) await UserSettings.updateUserSettings(settings._id, { $unset: { resetPassword : "" } } )
+    if(settings.resetPassword.bestBefore < new Date().getTime()) await UserSettings.unsetResetPasswordToken(settings._id)
 
-    return res.status(200).json({ resetPasswordIsaActual: settings.resetPassword.bestBefore < new Date() })
+    return res.status(200).json({ resetPasswordIsaActual: settings.resetPassword.bestBefore < new Date().getTime() })
   } catch(err) {
     return res.status(500).json({ error: err.message })
   }

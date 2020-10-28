abbccff0a2ed7b49d33087faa1d36db4a01fc8f2
Ilya Nesterov <j.nesterov1709@gmail.com>
2020-10-28 20:42:26 +0200

    set-new-password improved

--- api/controllers/set-new-password.js
+++ api/controllers/set-new-password.js
@@ -1,3 +1,4 @@
+const { ObjectID } = require('mongodb')
 const bycrypt = require('bcryptjs')
 const { UserSettings } = require('../../js/models/UserSettings')
 const { User } = require('../../js/models/User')
@@ -5,14 +6,20 @@ const { User } = require('../../js/models/User')
 
 module.exports = async function(req, res) { 
   try {
-    const { token , newPassword } = req.body
-    if(!token || !newPassword) return res.status(400).json({ error: 'something is missing' })
-   
-    const passwordReset = await UserSettings.getSpecificFields({ "resetPassword.token" : token }, { resetPassword: 1 })
-    if(!passwordReset) return res.status(404).json({ error: 'User not found' })
+    const { newPassword, userId, token } = req.body
+    if(!newPassword) return res.status(400).json({ error: 'new Password is missing' })
+    
+    let passwordReset 
+    token !== undefined
+      ? passwordReset = await UserSettings.getSpecificFields({ "resetPassword.token" : token }, { resetPassword: 1 })
+      : passwordReset = await User.getSpecificFields({ _id: new ObjectID(userId) })
 
-    await UserSettings.updateUserSettings(passwordReset._id.toString(), { $unset: { resetPassword : "" } })
-    if(passwordReset.resetPassword.bestBefore < new Date()) return res.status(200).json({ reset: false })
+    if(!passwordReset) return res.status(404).json({ error: 'User not found' })
+    
+    if(token){
+      await UserSettings.updateUserSettings(passwordReset._id.toString(), { $unset: { resetPassword : "" } })
+      if(passwordReset.resetPassword.bestBefore < new Date()) return res.status(200).json({ reset: false })
+    }
     
     const hashedPw = await bycrypt.hash(newPassword, 16)
     await User.updateUser(passwordReset._id.toString(), { $set : { password : hashedPw }})

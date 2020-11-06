9c2faccc1738b4e52118ef23e9456f52c321458c
Ilya Nesterov <j.nesterov1709@gmail.com>
2020-11-06 17:45:52 +0200

    view-usre modification

--- graphql/resolvers/view-user.js
+++ graphql/resolvers/view-user.js
@@ -16,10 +16,21 @@ module.exports = async function(userId, client){
     userId === client._id && throwAnError('Use fullUser endpoint instead', 400)
     updateUserActivity(client._id)
     const ifUserFollows = await Follower.findFollower(userId, client.username)
+
     if(!ifUserFollows) {
       await findOutIfSomeoneIsBlocked(client._id, userId) && throwAnError('Unable to view', 400)
-      !await findOutIfUserPublic(userId) && throwAnError('User is private', 400)
+      const ifUserIsPrivate = await findOutIfUserPublic(userId)
+      if(ifUserIsPrivate) {
+        let userToView = await User.getSpecificFields({ _id: new ObjectID(userId)}, { password: 0, _id: 0 })
+        let userIsPublic = await UserSettings.getSpecificFields({ _id: new ObjectID(userId) }, { public : 1, _id: 0 })
+        return {
+          ...userToView,
+          ...userIsPublic,
+          _id: userId,
+        }
+      }
     }
+
     const userToView = await User.getSpecificFields({ _id: new ObjectID(userId)}, { password: 0, _id: 0 })
     const userIsPublic = await UserSettings.getSpecificFields({ _id: new ObjectID(userId) }, { public : 1, _id: 0 })
     const userInfo = await UserInfo.getSpecificFields({ _id: new ObjectID(userId) }, { _id: 0 })

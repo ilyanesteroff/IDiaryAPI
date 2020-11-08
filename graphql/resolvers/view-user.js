89693a903ac51c4a42089d2fb55e618802814047
Ilya Nesterov <j.nesterov1709@gmail.com>
2020-11-08 20:45:37 +0200

    little fix in view-user

--- graphql/resolvers/view-user.js
+++ graphql/resolvers/view-user.js
@@ -13,23 +13,26 @@ const ifUserIsPublic = require('../../assistants/user/if-user-is-public')
 module.exports = async function(userId, client){
   try {
     !client && throwAnError('Authorization failed', 400)
-    userId === client._id && throwAnError('Use fullUser endpoint instead', 400)
-    updateUserActivity(client._id)
-    const ifUserFollows = await Follower.findFollower(userId, client.username)
 
-    if(!ifUserFollows) {
-      await findOutIfSomeoneIsBlocked(client._id, userId) && throwAnError('Unable to view', 400)
-      const userIsPublic = await ifUserIsPublic(userId)
-      if(!userIsPublic) {
-        let userToView = await User.getSpecificFields({ _id: new ObjectID(userId)}, { password: 0, _id: 0 })
-        let userIsPublic = await UserSettings.getSpecificFields({ _id: new ObjectID(userId) }, { public : 1, _id: 0 })
-        let lastSeen = await UserInfo.getSpecificFields({ _id: new ObjectID(userId) }, { _id: 0, lastSeen: 1 })
-        return {
-          ...userToView,
-          ...userIsPublic,
-          _id: userId,
-          lastSeen: lastSeen.lastSeen.toISOString(),
-          createdAt: userToView.createdAt.toISOString()
+    updateUserActivity(client._id)
+
+    if(userId !== client._id){
+      const ifUserFollows = await Follower.findFollower(userId, client.username)
+
+      if(!ifUserFollows) {
+        await findOutIfSomeoneIsBlocked(client._id, userId) && throwAnError('Unable to view', 400)
+        const userIsPublic = await ifUserIsPublic(userId)
+        if(!userIsPublic) {
+          let userToView = await User.getSpecificFields({ _id: new ObjectID(userId)}, { password: 0, _id: 0 })
+          let userIsPublic = await UserSettings.getSpecificFields({ _id: new ObjectID(userId) }, { public : 1, _id: 0 })
+          let lastSeen = await UserInfo.getSpecificFields({ _id: new ObjectID(userId) }, { _id: 0, lastSeen: 1 })
+          return {
+            ...userToView,
+            ...userIsPublic,
+            _id: userId,
+            lastSeen: lastSeen.lastSeen.toISOString(),
+            createdAt: userToView.createdAt.toISOString()
+          }
         }
       }
     }

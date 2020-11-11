d88076e7266aba0efe2f2f4735749afcf08144ad
Ilya Nesterov <j.nesterov1709@gmail.com>
2020-11-11 21:48:36 +0200

    typo

--- tests/messaging/test-get-conversation.js
+++ tests/messaging/test-get-conversation.js
@@ -5,7 +5,7 @@ const { client1 } = require('../utils/client')
   
 const view = async _ => {
   try {
-    const conv = await getConv('5f9d46c47f16750004a85759', client1)
+    const conv = await getConv('friend1', client1)
     console.log(conv)
   } catch(err) {
     console.log(err.message)

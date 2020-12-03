b756ab462e72c1505bacccfbc2d87a5a529731a5
Ilya Nesterov <j.nesterov1709@gmail.com>
2020-12-03 11:50:17 +0200

    commit

--- api/controllers/enough-available-space.js
+++ api/controllers/enough-available-space.js
@@ -3,9 +3,9 @@ const getDbStats = require('../../assistants/getDbStats')
 
 module.exports = async function(req, res){
   try {
-    let { documentsAmount } = req.body
-    //if(!documentsAmount) return res.status(400).json({ error: 'Amount of documents for creation is missing' })
-    documentsAmount = 1
+    const { documentsAmount } = req.body
+    if(!documentsAmount) return res.status(400).json({ error: 'Amount of documents for creation is missing' })
+
     const stats = await getDbStats()
 
     return res.status(200).json({ enough: documentsAmount * stats.avarage * 1.5 < stats.available })
